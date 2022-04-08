f0dea26c4c04aff011055aad52ed3bb1807d199b
mlodel <max.lodel@gmail.com>
2022-04-08 11:49:11 +0200

    block terminal outputs mpc

--- main.py
+++ main.py
@@ -23,38 +23,7 @@ from a2c_ppo_acktr.color_print import *
 
 import gym_collision_avoidance
 
-import sys
-
-from contextlib import contextmanager
-
-@contextmanager
-def stdout_redirected(to=os.devnull):
-    """
-    import os
-
-    with stdout_redirected(to=filename):
-        print("from Python")
-        os.system("echo non-Python applications are also supported")
-    """
-    fd = sys.stdout.fileno()
-
-    ##### assert that Python and C stdio write using the same file descriptor
-    #### assert libc.fileno(ctypes.c_void_p.in_dll(libc, "stdout")) == fd == 1
-
-    def _redirect_stdout(to):
-        sys.stdout.close()  # + implicit flush()
-        os.dup2(to.fileno(), fd)  # fd writes to 'to' file
-        sys.stdout = os.fdopen(fd, 'w')  # Python writes to fd
-
-    with os.fdopen(os.dup(fd), 'w') as old_stdout:
-        with open(to, 'w') as file:
-            _redirect_stdout(to=file)
-        try:
-            yield  # allow code to be run with the redirected stdout
-        finally:
-            _redirect_stdout(to=old_stdout)  # restore stdout.
-            # buffering and flags such as
-            # CLOEXEC may be different
+from a2c_ppo_acktr.blockstdout import stdout_redirected
 
 
 def main():
